a4ca8fa172643c44bb0720df5e84fb03a4328808
liuwang20144623 <1183862787@qq.com>
2018-11-27 19:59:51 +0800

    修改注释

--- MINST-LeNet-5.py
+++ MINST-LeNet-5.py
@@ -23,20 +23,24 @@ def conv2d(x, W):
 def max_pool_2x2(x):
 	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                         strides=[1, 2, 2, 1], padding='SAME')
-# 第一层卷积
+# 第一层：卷积层
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 x_image = tf.reshape(x, [-1,28,28,1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+
+# 第二层：池化层
 h_pool1 = max_pool_2x2(h_conv1)
 
-# 第二层卷积
+# 第三层：卷积层
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+
+# 第四层：池化层
 h_pool2 = max_pool_2x2(h_conv2)
 
-# 密集连接层
+# 第五层：全连接层
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
@@ -64,4 +68,4 @@ for i in range(20000):
 		print("step{}, training accuracy {}".format(i, train_accuracy))
 	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy{}"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+print("test accuracy：{}".format(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})))
